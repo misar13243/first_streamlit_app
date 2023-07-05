b3a8973431b13717b67834022d8a74cb60e57bff
misar13243 <138501520+misar13243@users.noreply.github.com>
2023-07-05 10:50:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-#import pandas
-#import requests
-#import snowflake.connector
-#from urllib.error import URLError
+##import pandas
+##import requests
+##import snowflake.connector
+##from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -13,9 +13,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+##import pandas
+##my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+##my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 ##fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -47,18 +47,18 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 ##streamlit.dataframe(fruityvice_normalized)
 
 # Stop here for troublshooting
-streamlit.stop()
+##streamlit.stop()
 
-import snowflake.connector
+##import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+##my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+##my_cur = my_cnx.cursor()
+##my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+##my_data_row = my_cur.fetchone()
+##streamlit.text("The fruit load list contains:")
+##streamlit.text(my_data_row)
 
-fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_add)
+##fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
+##streamlit.write('Thanks for adding ', fruit_add)
 
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+##my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
