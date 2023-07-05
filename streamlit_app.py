b1fcd13473b4137223b7bbc65f6319787ba64504
misar13243 <138501520+misar13243@users.noreply.github.com>
2023-07-05 10:48:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,14 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+##fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+##fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+##streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+##streamlit.header("Fruityvice Fruit Advice!")
 #try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
  # if not fruit_choice:
@@ -38,13 +38,13 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 #except URLError as e:
  #   streamlit.error()
   
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+##import requests
+##fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalise json output 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+##fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as table
-streamlit.dataframe(fruityvice_normalized)
+##streamlit.dataframe(fruityvice_normalized)
 
 # Stop here for troublshooting
 streamlit.stop()
