69a5de53be8fbab155f71db82bd66a7cf5e49e43
misar13243 <138501520+misar13243@users.noreply.github.com>
2023-07-05 10:33:48 +0100

    Update streamlit_app.py
    error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # import pandas
@@ -25,11 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruite to get information.")
+  else:
+      fruityvice_response = requests.get.("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruitvice_normalized = pandas.json_normalized(fruityvice_response.json())
+      streamlit.dataframe(fruitvice_normalized)
+ 
+except URLError as e:
+    streamlit.error()
+  
 # import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
